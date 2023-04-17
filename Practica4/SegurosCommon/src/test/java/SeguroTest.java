e60b210671407efd5f8e132a94f5e52c14e12403
Valvanuz <vgp670@alumnos.unican.es>
2023-04-17 16:41:11 +0200

    test del precio

--- Practica4/SegurosCommon/src/test/java/SeguroTest.java
+++ Practica4/SegurosCommon/src/test/java/SeguroTest.java
@@ -1,133 +1,104 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import es.unican.is2.SegurosCommon.daos.*;
 
 public class SeguroTest {
 	private Cliente c = new Cliente();
+	private Seguro s;
 	
 	
-
+	@BeforeEach
+	public void setUp() throws Exception {
+		s = new Seguro(Cobertura.TODORIESGO, 100, LocalDate.of(2022, 02, 12), c);
+	}
+	
 
 	//CASOS VALIDOS COBERTURA
+	@Test 
 	public void CoberturaTest() {
-
-		try {
-			Seguro s = new Seguro(Cobertura.TODORIESGO, 130, LocalDate.of(2023, 02, 12), c);
-			Cobertura cober = s.getCobertura();
+		Cobertura cober;
+			
+			s = new Seguro(Cobertura.TODORIESGO, 130, LocalDate.of(2023, 02, 12), c);
+			cober = s.getCobertura();
 			assertEquals(Cobertura.TODORIESGO, cober);
+			assertThrows(DatoIncorrectoException.class, () -> s.getCobertura());
+			
+			
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			cober = s.getCobertura();
+			assertEquals(Cobertura.TERCEROS, cober);
+			assertThrows(DatoIncorrectoException.class, () -> s.getCobertura());
 
-		}catch(DatoIncorrectoException e) {
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-		}
-
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
-			Cobertura cober = s.getCobertura();
-			assertEquals(Cobertura.TODORIESGO, cober);
-
-		}catch(DatoIncorrectoException e) {
-			fail("Caso2: No se deberia lanzar la excepcion\n");
-		}
-
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROSLUNAS, 92, LocalDate.of(2022, 02, 12), c);
-			Cobertura cober = s.getCobertura();
+		
+			s = new Seguro(Cobertura.TERCEROSLUNAS, 92, LocalDate.of(2022, 02, 12), c);
+			cober = s.getCobertura();
 			assertEquals(Cobertura.TERCEROSLUNAS, cober);
+			assertThrows(DatoIncorrectoException.class, () -> s.getCobertura());
 
-		}catch(DatoIncorrectoException e) {
-			fail("Caso3: No se deberia lanzar la excepcion\n");
-		}
 	}
 
 	//CASOS VALIDOS POTENCIA
+	@Test
 	public void potenciaTest() {
 
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
 			int pot = s.getPotencia();
 			assertTrue(pot > 90);
-
-		}catch(DatoIncorrectoException e) {
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-		}
+			assertThrows(DatoIncorrectoException.class, () -> s.getPotencia());
 	}
 
 	//CASOS VALIDOS PRECIO
+	@Test
 	public void precioTest() {
 
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
 			double prec = s.precio();
 			assertTrue(prec > 0);
-
-		}catch(DatoIncorrectoException e) {
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-		}
+			assertThrows(DatoIncorrectoException.class, () -> s.precio());
 	}
 	
 	//CASOS VALIDOS FECHA CONTRATACION
 	
 	public void fechaTest() {
-
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+		
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
 			LocalDate fecha = s.getFechaContratacion();
 			assertTrue(fecha.isBefore(LocalDate.now()));
 			assertTrue(fecha.isEqual(LocalDate.now()));
-
-		}catch(DatoIncorrectoException e) {
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-		}
+			assertThrows(DatoIncorrectoException.class, () -> s.getFechaContratacion());
 		
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
-			LocalDate fecha = s.getFechaContratacion();
-			assertTrue(fecha.getYear() - LocalDate.now().getYear() < 1 );	
-
-		}catch(DatoIncorrectoException e) {
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-		}
 		
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
-			LocalDate fecha = s.getFechaContratacion();
-			assertTrue(fecha.getYear() - LocalDate.now().getYear() > 1 );	
-			assertTrue(fecha.getYear() - LocalDate.now().getYear() < 2 );
-
-		}catch(DatoIncorrectoException e) {
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-		}
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			LocalDate fecha2 = s.getFechaContratacion();
+			assertTrue(fecha2.getYear() - LocalDate.now().getYear() < 1 );	
+			assertThrows(DatoIncorrectoException.class, () -> s.getFechaContratacion());
+		
+		
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			LocalDate fecha3 = s.getFechaContratacion();
+			assertTrue(fecha3.getYear() - LocalDate.now().getYear() > 1 );	
+			assertTrue(fecha3.getYear() - LocalDate.now().getYear() < 2 );
+			assertThrows(DatoIncorrectoException.class, () -> s.getFechaContratacion());
 		
 	}
 	
 	//CASOS VALIDOS CLIENTE
+	@Test
 	public void clienteTest() {
-		
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
 			boolean minus = c.getMinusvalia();
 			assertEquals(true, minus);
-		} catch (DatoIncorrectoException e){
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-			
-		}
+			assertThrows(DatoIncorrectoException.class, () -> c.getMinusvalia());
 		
-		try {
-			Seguro s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
-			boolean minus = c.getMinusvalia();
-			assertEquals(false, minus);
-		} catch (DatoIncorrectoException e){
-			fail("Caso1: No se deberia lanzar la excepcion\n");
-			
-		}
+			s = new Seguro(Cobertura.TERCEROS, 110, LocalDate.of(2022, 02, 12), c);
+			boolean minus2 = c.getMinusvalia();
+			assertEquals(false, minus2);
+			assertThrows(DatoIncorrectoException.class, () -> c.getMinusvalia());
+		
 	}
-	
-	//CASOS NO VALIDOS
-	
-	
-
 
 }
